6422a9cd21eb924e246bffe1945992bc8784a3b9
nacho <fan01856472@gmail.com>
2021-09-18 18:34:26 +0800

    handle basic exception

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -14,8 +14,12 @@ module Rulers
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
-      [200, { "Content-Type" => "text/html" }, [text]]
+      begin
+        text = controller.send(act)
+        [200, { "Content-Type" => "text/html" }, [text]]
+      rescue => exception
+        [404, { "Content-Type" => "text/html" }, ["Somthing wrong"]]
+      end
     end
   end
 
