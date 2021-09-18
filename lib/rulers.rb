3449d317d8eda5e688b8a2ed12b5f1a522b97dc7
nacho <fan01856472@gmail.com>
2021-09-18 17:59:47 +0800

    tell controller and action

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 require "rulers/array"
-require_relative "rulers/version"
+require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
-      [200, { "Content-Type" => "text/html" }, ["Hello from Ruby on Rulers!"]]
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, { "Content-Type" => "text/html" }, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
