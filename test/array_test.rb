4df4e6bc2ed5b07f87665e3bbb4a66457dcaa47a
nacho <fan01856472@gmail.com>
2021-10-03 13:36:20 +0800

    add test case

--- test/array_test.rb
+++ test/array_test.rb
@@ -1,7 +1,13 @@
 require_relative "test_helper"
 
 class TestArray < Minitest::Test
+
   def test_sum_return_value
     assert ([1,2,3].sum) == 6
   end
+
+  def test_sum_return_zero
+    assert ([].sum) == 0
+  end
+
 end
