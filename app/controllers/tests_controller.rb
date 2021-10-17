c8325aaedbb1995b910ac05f1a0b7f1b971e08ae
nacho <fan01856472@gmail.com>
2021-10-17 15:48:15 +0800

    add FileModel.all

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -11,4 +11,9 @@ class TestsController < Rulers::Controller
     quote_1 = FileModel.find(1)
     render :hello_quote, :obj => quote_1
   end
+
+  def more_quotes
+    quotes = FileModel.all
+    render :more_quotes, :quotes => quotes
+  end
 end
--- test/application_test.rb
+++ test/application_test.rb
@@ -42,4 +42,14 @@ class ApplicationTest < Minitest::Test
     assert body["A penny saved is a penny earned."]
   end
 
+  def test_more_quotes
+    get "/tests/more_quotes"
+
+    assert last_response.ok?
+    body = last_response.body
+    assert body["A penny saved is a penny earned."]
+    assert body["The greatest glory in living lies not in never falling, but in rising every time we fall."]
+    assert body["Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."]
+  end
+
 end
